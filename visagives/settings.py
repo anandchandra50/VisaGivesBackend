d38ca786c4f765996badb8f55865f18bc5034620
Anand Chandra <anandchandra50@gmail.com>
2020-07-01 17:29:24 -0700

    Deployment Prep

--- visagives/settings.py
+++ visagives/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,3 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
+CORS_ORIGIN_ALLOW_ALL = True  
